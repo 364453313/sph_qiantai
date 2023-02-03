94db615f89f50f49b90eaea80a67cf23932f49ba
zn yang <364453313@qq.com>
2023-02-03 16:19:39 +0800

    Home首页静态组件拆分

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,10 +1,33 @@
 <template>
-  <div>我是首页fdssafdsfa</div>
+  <div>
+    <!-- 使用三级联动全局组件,已经注册为全局组件,不需要引入 -->
+    <TypeNav />
+    <ListContainer />
+    <TodayRecommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <Brand />
+  </div>
 </template>
 
 <script>
+import ListContainer from '@/pages/Home/ListContainer'
+import TodayRecommend from '@/pages/Home/TodayRecommend'
+import Rank from '@/pages/Home/Rank'
+import Like from '@/pages/Home/Like'
+import Floor from '@/pages/Home/Floor'
+import Brand from '@/pages/Home/Brand'
 export default {
-
+    name:'Home',
+    components:{
+        ListContainer,
+        TodayRecommend,
+        Rank,
+        Like,
+        Floor,
+        Brand
+    }
 }
 </script>
 
