b15e9f8e0a23d3279660ca32ca5f91c2d26a50a1
yangyuewei <364453313@qq.com>
2023-02-07 22:49:04 +0800

    api接口统一管理

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+//这个模块进行API统一管理
+import requests from "./request";
+
+//三级联动的接口
+///api/product/getBaseCategoryList  get 无参数
+
+//发请求；axios发请求返回的结果是Promise对象
+export const reqCategoryList = ()=>requests({url:'/product/getBaseCategoryList',method:'get'})
