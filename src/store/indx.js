77f2e5590e68f1b2721d3eaf72ec22ff59a0b3e8
zn yang <364453313@qq.com>
2023-02-09 17:30:36 +0800

    vuex

---
+++ src/store/indx.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+//需要使用一次插件
+Vue.use(Vuex)
+
+//对外暴露Store类的实例
+export default new Vue.Store({
+    state:{
+        
+    },
+    mutations:{
+
+    },
+    actions:{
+
+    },
+    getters:{
+
+    }
+})
