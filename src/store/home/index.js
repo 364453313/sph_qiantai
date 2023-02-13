db4ac9aac75f4da4e0dd2c0823b700bdc91edc5e
yangyuewei <364453313@qq.com>
2023-02-13 23:19:23 +0800

    三级联动数据

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,7 +1,23 @@
+import { reqCategoryList } from '@/api'
 //search模块小仓库
-const state = {}
-const mutations = {}
-const actions = {}
+const state = {
+    //state中数据默认初始化根据接口返回值初始化
+    categoryList: []
+}
+const mutations = {
+    CATEGORYLIST(state, categoryList) {
+        state.categoryList = categoryList
+    }
+}
+const actions = {
+    //通过API里面的接口函数调用，向服务器发送请求，获取服务器数据
+    async categoryList({ commit }) {
+        let result = await reqCategoryList()
+        if (result.code == 200) {
+            commit("CATEGORYLIST", result.data)
+        }
+    }
+}
 const getters = {}
 export default {
     state,
