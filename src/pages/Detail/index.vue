0d5469fa804dbfec2088292402c91c6becc8d57c
zn yang <364453313@qq.com>
2023-03-29 17:09:46 +0800

    detail售卖属性展示

--- src/pages/Detail/index.vue
+++ src/pages/Detail/index.vue
@@ -18,7 +18,7 @@
                     <!--放大镜效果-->
                     <Zoom :skuImageList="skuImageList" />
                     <!-- 小图列表 -->
-                    <ImageList />
+                    <ImageList :skuImageList="skuImageList" />
                 </div>
                 <!-- 右侧选择区域布局 -->
                 <div class="InfoWrap">
@@ -77,29 +77,11 @@
                     <div class="choose">
                         <div class="chooseArea">
                             <div class="choosed"></div>
-                            <dl>
-                                <dt class="title">选择颜色</dt>
+                            <dl v-for="(item,index) in spuSaleAttrList" :key="item.id">
+                                <dt class="title">{{item.saleAttrName}}</dt>
                                 <dd changepirce="0" class="active">金色</dd>
                                 <dd changepirce="40">银色</dd>
                                 <dd changepirce="90">黑色</dd>
-                            </dl>
-                            <dl>
-                                <dt class="title">内存容量</dt>
-                                <dd changepirce="0" class="active">16G</dd>
-                                <dd changepirce="300">64G</dd>
-                                <dd changepirce="900">128G</dd>
-                                <dd changepirce="1300">256G</dd>
-                            </dl>
-                            <dl>
-                                <dt class="title">选择版本</dt>
-                                <dd changepirce="0" class="active">公开版</dd>
-                                <dd changepirce="-1000">移动版</dd>
-                            </dl>
-                            <dl>
-                                <dt class="title">购买方式</dt>
-                                <dd changepirce="0" class="active">官方标配</dd>
-                                <dd changepirce="-240">优惠移动版</dd>
-                                <dd changepirce="-390">电信优惠版</dd>
                             </dl>
                         </div>
                         <div class="cartWrap">
@@ -379,7 +361,7 @@ export default {
         this.$store.dispatch("getGoodInfo", this.$route.params.skuid);
     },
     computed: {
-        ...mapGetters(["categoryView", "skuInfo"]),
+        ...mapGetters(["categoryView", "skuInfo","spuSaleAttrList"]),
         skuImageList(){
             return this.skuInfo.skuImageList || []
         }
