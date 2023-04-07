5838dff1bab90a69394562e8c2d17b103ee9d7b0
zn yang <364453313@qq.com>
2023-04-07 15:47:10 +0800

    购买产品个数的操作

--- src/pages/Detail/index.vue
+++ src/pages/Detail/index.vue
@@ -113,9 +113,26 @@
                         </div>
                         <div class="cartWrap">
                             <div class="controls">
-                                <input autocomplete="off" class="itxt" />
-                                <a href="javascript:" class="plus">+</a>
-                                <a href="javascript:" class="mins">-</a>
+                                <input
+                                    autocomplete="off"
+                                    class="itxt"
+                                    v-model="skuNum"
+                                    @change="changeSkuNum"
+                                />
+                                <a
+                                    href="javascript:"
+                                    class="plus"
+                                    @click="skuNum++"
+                                    >+</a
+                                >
+                                <a
+                                    href="javascript:"
+                                    class="mins"
+                                    @click="
+                                        skuNum > 1 ? skuNum-- : (skuName = 1)
+                                    "
+                                    >-</a
+                                >
                             </div>
                             <div class="add">
                                 <a href="javascript:">加入购物车</a>
@@ -378,7 +395,11 @@ import { mapGetters } from "vuex";
 
 export default {
     name: "Detail",
-
+    data() {
+        return {
+            skuNum: 1,
+        };
+    },
     components: {
         ImageList,
         Zoom,
@@ -395,10 +416,18 @@ export default {
     },
     methods: {
         changeActive(saleAttrValue, arr) {
-            arr.forEach(element => {
-                element.isChecked = '0'
+            arr.forEach((element) => {
+                element.isChecked = "0";
             });
-            saleAttrValue.isChecked = '1'
+            saleAttrValue.isChecked = "1";
+        },
+        changeSkuNum(event) {
+            let value = event.target.value * 1;
+            if (isNaN(value) || value < 1) {
+                this.skuNum = 1
+            }else{
+                this.skuNum = parseInt(value)
+            }
         },
     },
 };
